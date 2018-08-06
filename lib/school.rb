10cb36dbb1a27be0bfc0c523a9208358ebb279fa
Tammy Metz <tammy@tammymetz.com>
2018-08-06 22:54:55 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+
+class School
+
+  def initialize
+    self.roster = {}
+  end
+
+  def add_student(name, grade)
+    if !self.roster.key?(grade)
+      self.roster[grade] = []
+    end
+    self.roster[grade] << name
+    self.roster
+  end
+
+end
